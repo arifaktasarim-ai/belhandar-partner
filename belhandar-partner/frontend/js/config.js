1a465ae2226d756e1994d96c9ee67fd14310a3ca
Arif Aktaş <arifaktasarim@gmail.com>
2026-08-23 15:57:46 +0300

    Configure production API URL

--- belhandar-partner/frontend/js/config.js
+++ belhandar-partner/frontend/js/config.js
@@ -2,6 +2,6 @@
 // Gelistirme icin varsayilan: http://localhost:4000
 // Production'a alirken bu degeri kendi sunucu adresinizle degistirin.
 window.BELHANDAR_CONFIG = {
-  API_BASE_URL: 'http://localhost:4000/api',
+  API_BASE_URL: 'https://belhandar-partner.onrender.com/api',
   BRAND_NAME: 'Belhandar',
 };
